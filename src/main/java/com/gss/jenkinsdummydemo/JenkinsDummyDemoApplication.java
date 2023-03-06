cfe99c274585de21acfa6646b3407112a0a64d3f
manish singh <manish.singh@rstartec.com>
2023-03-06 15:21:31 +0530

    init commit

---
+++ src/main/java/com/gss/jenkinsdummydemo/JenkinsDummyDemoApplication.java
@@ -0,0 +1,13 @@
+package com.gss.jenkinsdummydemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JenkinsDummyDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JenkinsDummyDemoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/gss/jenkinsdummydemo/JenkinsDummyDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gss.jenkinsdummydemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JenkinsDummyDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
